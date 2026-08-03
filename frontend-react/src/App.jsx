f2117c9e80149d1df36b0b35ddc35a2d6c5f9c5a
hosurVijay <hosurvijayendra@gmail.com>
2026-08-04 01:19:23 +0530

    Vite react added

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return<>
+    <h1 class="text-3xl font-bold underline text-blue-600">
+          Tailwind is Working!
+  </h1>
+  </>
+}
+
+export default App
